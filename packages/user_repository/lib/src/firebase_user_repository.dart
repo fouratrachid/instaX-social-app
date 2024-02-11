235741154b933969bc2faad9a6b7ecf0db9a21f8
foufou <rachidfourat2@gmail.com>
2024-02-11 16:55:31 +0100

    Created user repo

---
+++ packages/user_repository/lib/src/firebase_user_repository.dart
@@ -0,0 +1,11 @@
+import 'user_repo.dart';
+
+class FirebaseUserRepository implements UserRepository {
+  // Sign In
+
+  // Sign out
+
+  //Sign Up
+
+  // Reset Password
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:insta_x/app.dart';
 
 import 'package:insta_x/main.dart';
 
